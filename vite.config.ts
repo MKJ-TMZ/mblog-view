4e3f139f26664ec3f37349ca08e002faea65ea74
MTCode <1318844659@qq.com>
2022-02-12 23:00:02 +0800

    添加别名

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src")
+      "@": resolve(__dirname, "src"),
+      "assets": resolve(__dirname, "src/assets"),
     },
     extensions: ['.js', '.json', '.ts']
   },
